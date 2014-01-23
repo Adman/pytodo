47d9650a178270263a7999de22ad82e6835394a4
Ado Matejov <a.matejov@centrum.sk>
2014-01-23 21:15:14 +0100

    started working on class

--- todo_notes.py
+++ todo_notes.py
@@ -9,10 +9,63 @@ import os
 
 TASKS_PATH = os.path.join(os.path.dirname(__file__), "tasks.txt")
 
+
+class Task:
+    def __init__(self, text, ID, priority):
+        self.text = text
+        self.ID = ID
+        self.priority = priority
+
+
+class ToDoNotes:
+    def __init__(self):
+        if not os.path.exists(os.path.dirname(TASKS_PATH)):
+            os.mkdir(os.path.dirname(TASKS_PATH))
+
+        if not os.path.isfile(TASKS_PATH):
+            f = open(TASKS_PATH, 'w')
+            f.close()
+
+    def add_task(self):
+        """Adds a task to the .txt file"""
+        pass
+
+    def rm_task(self):
+        """Removes a task from .txt file"""
+        pass
+
+    def show_notification(self):
+        """Shows notification window"""
+        pass
+
+    def print_help(self):
+        """Prints available commands on the screen"""
+        pass
+
+    def process(self, args):
+        cmd = args[0].lower() if len(args) else 0
+
+        if len(args):
+            if cmd == 'help':
+                self.print_help()
+                sys.exit(1)
+            elif cmd == 'add':
+                self.add_task(args[1:])
+            elif cmd == 'rm' or cmd == 'remove' or cmd == 'done':
+                self.rm_task(args[1:])
+            else:
+                self.print_help()
+                sys.exit(1)
+
+        self.show_notification()
+
+    def _open_file(self, mode="r"):
+        self.f = open(TASKS_PATH, mode)
+
+    def _close_file(self):
+        self.f.close()
+
 def add_task(args):
-    """
-        Adds task to the .txt file
-    """
     f = open(TASKS_PATH, 'a')
     task = ''
     for i in args:
@@ -22,9 +75,6 @@ def add_task(args):
     f.close()
 
 def rm_task(args):
-    """
-        Removes task from .txt file
-    """
     fr = open(TASKS_PATH, 'r')
     lines = fr.readlines()
     fr.close()
@@ -49,7 +99,7 @@ def show_window():
     """
         Reads tasks from the .txt file and shows them on the screen.
     """
-    f = open(TASKS_PATH, 'r')
+    f = open(TASKS_PATH, 'r+')
     tasks = f.read()
 
     #print tasks into notification window
@@ -76,7 +126,7 @@ def print_help():
     print "Available arguments:"
     print "	help - show available commands"
     print "	add <task> - add a task to the todo list"
-    print "	rm <number> - remove a task from the todo list"
+    print "	rm/remove/done <number> - remove a task from the todo list"
 
 def main(args):
     if not os.path.exists(os.path.dirname(TASKS_PATH)):
@@ -86,13 +136,15 @@ def main(args):
         f = open(TASKS_PATH, 'w')
         f.close()
 
+    cmd = args[0].lower() if len(args) else 0
+
     if len(args):
-        if args[0].lower() == 'help':
+        if cmd == 'help':
             print_help()
             sys.exit(1)
-        elif args[0].lower() == 'add':
+        elif cmd == 'add':
             add_task(args[1:])
-        elif args[0].lower() == 'rm' or args[0].lower() == 'remove':
+        elif cmd == 'rm' or cmd == 'remove' or cmd == 'done':
             rm_task(args[1:])
         else:
             print_help()
